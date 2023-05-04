54837a87950f9e20956d11b4a89b80bf0146ef0d
Buttu <129733301+iButtu@users.noreply.github.com>
2023-05-04 15:20:27 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -105,7 +105,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     if GITHUB_REPO and OWNER:
         buttons.append(
             [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
+                InlineKeyboardButton(text=_["S_B_7"], url=f"{APPLE_EXCLUSIVE}"),
                 InlineKeyboardButton(
                     text=_["S_B_6"], url=f"{GITHUB_REPO}"
                 ),
@@ -117,6 +117,14 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 [
                     InlineKeyboardButton(
                         text=_["S_B_6"], url=f"{GITHUB_REPO}"
+                    ),
+                ]
+            )
+                    if APPLE_EXCLUSIVE:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text=_["S_B_7"], url=f"{APPLE_EXCLUSIVE}"
                     ),
                 ]
             )
